08eb9f3829a5d80bf889c5ba0f56695088afdc2b
jhs1v1 <67949783+jhs1v1@users.noreply.github.com>
2020-11-09 18:29:44 +0900

    Term_project_1

---
+++ Term_1/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Random random = new Random();
+		
+		int N = scan.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0; i<N; i++) {
+			arr[i] = random.nextInt();
+		}
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		scan.close();
+		
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int sum = 0;
+
+		for(int i = a-1; i<b; i++) {
+			if(arr[i] < min) {
+				min = arr[i];
+			}
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+			sum += arr[i];
+		}
+		System.out.println("최소값: "+min+" , 최대값: "+max+" , 합: "+sum);
+	}
+}
